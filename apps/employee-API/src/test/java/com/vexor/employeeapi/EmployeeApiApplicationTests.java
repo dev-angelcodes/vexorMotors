02c6615d152c7141f8314dab5ef301ecd33a7953
Angel Fernandez <angelfernandezblanco16@gmail.com>
2026-02-03 14:42:07 +0100

    Monolite distribuited estructure

---
+++ apps/employee-API/src/test/java/com/vexor/employeeapi/EmployeeApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.vexor.employeeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class EmployeeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
